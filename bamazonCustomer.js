267a212f9ad949b34641eae34ea38f7316c93c23
Alex Rocha <rocha8524@gmail.com>
2019-11-08 18:41:54 -0500

    Working on purchase function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,7 @@
 // Create variables that connect npm packages
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+require("console.table");
 
 // Create the connection information for the sql database
 var connection = mysql.createConnection({
@@ -20,32 +21,20 @@ var connection = mysql.createConnection({
 // Connect to the mysql server and sql database
 connection.connect(function (error) {
     if (error) throw error;
-    console.log("Connected as id " + connection.threadId);
-    showAllItems();
+    welcomeShopper()
 });
-
-function showAllItems() {
-    connection.query("SELECT * FROM products",
-        function (error, response) {
-            if (error) throw error;
-            for (var i = 0; i < response.length; i++) {
-                console.log("\n" + response[i].id + " | " + response[i].product_name + " | " + response[i].department_name + " | " + response[i].price + " | " + response[i].stock_quantity + "\n");
-            };
-            welcomeShopper();
-        });
-}
 
 function welcomeShopper() {
     inquirer.prompt({
-        name: "store",
+        name: "choice",
         type: "rawlist",
-        message: "Would you like to shop at Bamazon today?",
+        message: "Welcome to Bamazon. Are you interested in shopping with us today?",
         choices: ["Yes", "No"]
     })
         .then(function (answer) {
             if (answer.store === "Yes") {
-                console.log("\n" + "Glad you are doing buisness with us today." + "\n" + "We have a great selection of goods in stock.");
-                startShopping();
+                console.log("\n" + "We are glad you are doing buisness with us today." + "\n" + "We have a great selection of goods in stock." + "\n");
+                showAllItems();
             } else {
                 console.log("Just browsing, get lost you freeloader!");
                 connection.end();
@@ -53,6 +42,69 @@ function welcomeShopper() {
         });
 }
 
-function startShopping() {
+function showAllItems() {
+    connection.query("SELECT * FROM products",
+        function (error, response) {
+            if (error) throw error;
+            console.table(response);
+            startShopping()
+        });
+}
+
+function startShopping(storeInventory) {
+    inquirer.prompt([{
+        name: "choose",
+        type: "input",
+        message: "What item would you like to purchase today?",
+        validate: function (value) {
+            if (isNaN(value) === false) {
+                return true;
+            }
+            return false;
+        }
+    }]).then(function (value) {
+        var chooseID = parseInt(value.choose);
+        var item = checkStoreInventory(chooseID, storeInventory);
+        if (item) {
+            selectQuantity(item);
+        } else {
+            console.log("Sorry, we don't have this item available. Please check back with us tomorrow!");
+            showAllItems();
+        }
+    });
+}
+
+function selectQuantity(product) {
+    inquirer.prompt([{
+        name: "quantity",
+        type: "input",
+        message: "How many would you like to purchase today?",
+        validate: function (value) {
+            return value > 0;
+        }
+    }
+    ]).then(function (value) {
+        var quantity = parseInt(value.quantity);
+
+        if (quantity > product.stock_quantity) {
+            console.log("Not enough in stock. Please purchase something else.")
+        } else {
+            makePurchase()
+        }
+    });
+}
+
+function makePurchase(product, quantity) {
+    connection.query("UPDATE products SET stock_quantity = stock_quantity - ? WHERE id = ?",
+        [quantity, product.item_id],
+        function (error, response) {
+            if (error) throw error;
+            console.log(response.affectedRows + " products updated!\n");
+            connection.end();
+        }
+    );
+}
+
+function checkStoreInventory() {
 
 }
